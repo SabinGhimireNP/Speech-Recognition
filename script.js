21bc77abd1fdf76307334897afe5e8ba447409bd
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-07 23:06:33 +0545

    Formatting the script

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
+// SpeechRecognition
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
-
 const recognition = new SpeechRecognition();
-recognition.interimResults = true;  
+recognition.interimResults = true;
 
+// p-tag Creation
 let p = document.createElement("p");
 const words = document.querySelector(".words");
 words.appendChild(p);
 recognition.start();
-recognition.addEventListener("result", (e) => {
-  // console.log(e);
+
+// Functions
+function addContent() {
   const transcript = Array.from(e.results)
     .map((result) => result[0])
     .map((result) => result.transcript)
     .join("");
+  // console.log(transcript);
   // console.log(e);
-  p.innerHTML = transcript;
-  if (e.results[0].isFinal) {
-    p = document.createElement("p");
-    words.appendChild(p);
-  }
-});
+}
+
+// Event Listners
+recognition.addEventListener("result", addContent);
 recognition.addEventListener("end", () => recognition.start());
